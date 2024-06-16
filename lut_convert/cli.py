8be36eb2bef8b7f890e28a37780af25e24306512
Christian Wygoda <christian.wygoda@wygoda.net>
2024-06-16 19:24:23 +0200

    feat: don't replace spaces with underscores

--- lut_convert/cli.py
+++ lut_convert/cli.py
@@ -12,6 +12,6 @@ from lut_convert.convert import convert
 )
 @option("--size", type=int, default=64)
 def cli(input: Path, size: int):
-    out = input.parent / f"{input.stem.replace(' ', '_')}.cube"
+    out = input.parent / f"{input.stem}.cube"
     with out.open("w") as out:
         convert(input, out, size, input.stem)
